6aebd4793c3986caf8101cef7b1577866b733730
Eva Brechum <Brechum.Eva@gmail.com>
2022-06-22 21:52:47 -0700

    continuing to practice math operators and variables

---
+++ main.js
@@ -0,0 +1,7 @@
+// Create a variable named myAge,
+// and set it equal to your age as a number.
+
+var myAge = 32;
+
+// Create a variable named earlyYears and save the value 2 to it
+var earlyYears = 2;
