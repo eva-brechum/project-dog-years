18ff704e6fb6a9e46ea303ff34c29683b7270ca6
Eva Brechum <Brechum.Eva@gmail.com>
2022-06-22 22:00:06 -0700

    math operators and variables

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ var myAge = 32;
 
 // Create a variable named earlyYears and save the value 2 to it
 var earlyYears = 2;
+
+// Use the multiplication assignment operator to multiply the value saved to earlyYears
+// by 10.5 and reassign it to earlyYears.
+earlyYears *= 10.5;
+
+// take the myAge variable, and subtract 2 from it.
+var laterYears = myAge - 2;
+
+laterYears * 4;
