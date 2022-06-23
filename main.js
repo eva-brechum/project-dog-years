ad44116fe4ae03ad1cb322c0a5946b5fff649d4e
Eva Brechum <Brechum.Eva@gmail.com>
2022-06-22 22:33:13 -0700

    math operators and variables

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
 // Create a variable named myAge,
 // and set it equal to your age as a number.
 
-var myAge = 32;
+const myAge = 24;
 
 // Create a variable named earlyYears and save the value 2 to it
-var earlyYears = 2;
+let earlyYears = 2;
 
 // Use the multiplication assignment operator to multiply the value saved to earlyYears
 // by 10.5 and reassign it to earlyYears.
 earlyYears *= 10.5;
 
 // take the myAge variable, and subtract 2 from it.
-var laterYears = myAge - 2;
+let laterYears = myAge - 2;
 
-laterYears * 4;
+// Multiply the laterYears variable by 4 to calculate the number of dog years
+// accounted for by your later years. Use the multiplication assignment
+// operator to multiply and assign in one step.
+
+laterYears *= 4;
+//
+// console.log(laterYears);
+// console.log(earlyYears);
+
+// Add earlyYears and laterYears together
+// and store that in a variable named myAgeInDogYears.
+let myAgeInDogYears = earlyYears + laterYears;
+
+// Write your name as a string, call its built-in method .toLowerCase()
+// and store the result in a variable called myName.
+
+let myName = "Eva".toLowerCase();
+// console.log(myName)
+
